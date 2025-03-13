6c7238f8766eab447cef441ce9887814bc34ab5c
DaniDItzMe <td.danix@gmail.com>
2025-03-13 16:23:05 +0100

    Changes

--- client.js
+++ client.js
@@ -1,11 +1,9 @@
 window.addEventListener("DOMContentLoaded", e=>{
     
+
     const query1 = "http://localhost:3333/users?id=12&token=abc";
     const query2 = "http://localhost:3333/users/5678/asdf";
     const query3 = "http://localhost:3333/users";
-    // const button1 = document.querySelector("#uno");
-    // const button2 = document.querySelector("#due");
-    // const button3 = document.querySelector("#tre");
 
     const buttons = document.querySelectorAll(".bottone");
     const display = document.querySelector("#display");
@@ -15,33 +13,51 @@ window.addEventListener("DOMContentLoaded", e=>{
 
             let target = e.target;
             console.log(`You clicked on button ${target.id}`);
-            fetch(query1)
-            .then(response =>{
-                console.log(response);
-                display.innerHTML = response.status+" "+ response.statusText;
-            })
+            switch(e.target.id){
+
+                case "uno":
+                    fetch(query1)
+                    .then(response =>{
+                        console.log(response);
+                        display.innerHTML = response.status+" "+ response.statusText;
+                    })
+                break;
+
+                case "due":
+                    fetch(query2)
+                    .then(response =>{
+                        console.log(response);
+                        display.innerHTML = response.status+" "+ response.statusText;
+                    })
+                break;
+
+                case "tre":
+
+                    const request = {
+                        method: "post",
+                        header: {
+                            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                        },
+                        body: new URLSearchParams({
+                            id: 5,
+                            token: "56svfgfdt782f" 
+                        })
+                    }
+
+                    fetch("http://localhost:3333/users", request)
+                    .then(response =>{
+
+                        console.log(response);
+                        display.innerHTML = response.status + " " + response.statusText;
+
+                    })
+            
+                break;
+
+            }
+            
         })
 
     })
 
-    // button1.addEventListener("click", e=>{
-
-    //     b = e.target;
-    //     console.log(`Hai cliccato il bottone ${b.id}`);
-        
-    // })
-
-    // button2.addEventListener("click", e=>{
-
-    //     b = e.target;
-    //     console.log(`Hai cliccato il bottone ${b.id}`);
-
-    // })
-
-    // button3.addEventListener("click", e=>{
-        
-    //     b = e.target;
-    //     console.log(`Hai cliccato il bottone ${b.id}`);
-  
-    // })
 })
--- server.js
+++ server.js
@@ -1,13 +1,38 @@
-const { log } = require("console");
 const express = require("express");
 const app = express();
 const port = 3333;
 
 app.use(express.static(__dirname));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 app.get("/", (req,res)=>{
 
     res.sendFile(__dirname+"/client.html");
+
+})
+
+app.get("/users", (req,res)=>{
+
+    let {id, token} = req.query; 
+    console.log(`ID: ${id} TOKEN: ${token}`);
+    res.status(200).send("Ciao");
+
+})
+
+app.get('/users/:id/:token', (req, res) => {
+
+    let {id, token} = req.params;
+    console.log(`ID: ${id} TOKEN: ${token}`);
+    res.status(200).send("OK");
+
+});
+
+app.post("/users", (req, res)=>{
+
+    let {id, token} = req.body;
+    console.log(`ID: ${id} TOKEN: ${token}`);
+    res.status(200).send();
 
 })
 
